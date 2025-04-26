5a4c9c5149c52c36234a3fd8daf18823f5ffbf35
Ololade <73138104+zololade@users.noreply.github.com>
2025-04-26 23:33:04 +0100

    refactor: adjust hamburger button size and improve transition effects

--- frontend/src/components/hamburger.jsx
+++ frontend/src/components/hamburger.jsx
@@ -5,22 +5,22 @@ export default function Hamburger(props) {
 
   return (
     <button
-      className="group flex flex-col items-center justify-center w-10 h-10 space-y-1 border rounded shadow shadow-gray-600"
+      className="group flex flex-col items-center justify-center w-7 h-7 space-y-1 border rounded shadow shadow-gray-600"
       onClick={handleClick}
     >
       <span
-        className={`w-6 h-0.5 bg-black transition-transform duration-300 ${
-          open ? "rotate-45 translate-y-2" : ""
+        className={`w-4 h-0.5 bg-black transition-transform duration-300 ${
+          open ? " translate-y-[0.378rem] rotate-45" : "translate-y-0.5"
         }`}
       />
       <span
-        className={`w-6 h-0.5 bg-black transition-opacity duration-300 ${
+        className={`w-3 h-0.5 bg-black transition-opacity duration-300 ${
           open ? "opacity-0" : ""
         }`}
       />
       <span
-        className={`w-6 h-0.5 bg-black transition-transform duration-300 ${
-          open ? "-rotate-45 -translate-y-2" : ""
+        className={`w-4 h-0.5 bg-black transition-transform duration-300 ${
+          open ? " -translate-y-[0.378rem] -rotate-45" : "-translate-y-0.5"
         }`}
       />
     </button>
